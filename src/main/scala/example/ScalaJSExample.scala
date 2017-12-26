f8d9540fcac243773e4ccdaa7128cfa584b28e5b
Howard <howard@TSC1s-MBP.attlocal.net>
2017-12-26 18:46:57 -0500

    Bouncing ball works

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -3,6 +3,9 @@ import scala.scalajs.js.annotation._
 import org.scalajs.dom
 import org.scalajs.dom.html
 import scala.util.Random
+import scala.math
+import scala.scalajs.js
+
 
 case class Point(x: Int, y: Int){
   def +(p: Point) = Point(x + p.x, y + p.y)
@@ -13,32 +16,66 @@ case class Point(x: Int, y: Int){
 object ScalaJSExample {
   @JSExport
   def main(canvas: html.Canvas): Unit = {
-    val ctx = canvas.getContext("2d")
-                    .asInstanceOf[dom.CanvasRenderingContext2D]
+    val renderer = canvas.getContext("2d")
+      .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
+    canvas.width = 50 
+    canvas.height = 70
 
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
+    
+
+
+
+    //vals
+    var xspeed = 3.3
+    var yspeed = 3.3
+
+
+
+
+    var playerY = canvas.height / 2.0
+    var playerX = canvas.width / 2.0
+
+    def run() = {
+      playerX += xspeed
+
+      playerY += yspeed
+
+
+      
+
+
+      if(playerX > canvas.width || playerX < 0) xspeed *= -1
+      if(playerY > canvas.height || playerY < 0) yspeed *= -1
+
+
+      
+     
+
+      //animate
+      renderer.fillRect(playerX, playerY, 10, 10)
+      
+      
     }
 
-    def run = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
+    def trueRun() = {
+      
+      renderer.clearRect(0, 0, canvas.width, canvas.height)
+      run()
     }
 
-    dom.window.setInterval(() => run, 50)
+    js.timers.setInterval(60) {
+      trueRun()
+    }
+    
+
+
+
+
   }
+
+
+
+
+
 }
